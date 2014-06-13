c99a5f94322b695507b5f03dd71246a949ccb56b
alonso <alons182@hotmail.com>
2014-06-12 20:18:31 -0600

    update joomla 3.3.1

--- logs/joomla_update.php
+++ logs/joomla_update.php
@@ -23,3 +23,11 @@
 2014-04-30T22:28:23+00:00	INFO	update	Deleting removed files and folders.
 2014-04-30T22:28:24+00:00	INFO	update	Cleaning up after installation.
 2014-04-30T22:28:24+00:00	INFO	update	Update to version 3.3.0 is complete.
+2014-06-13T02:11:09+00:00	INFO	update	Update started by user Super User (797). Old version is 3.3.0.
+2014-06-13T02:11:10+00:00	INFO	update	Downloading update file from .
+2014-06-13T02:11:20+00:00	INFO	update	File Joomla_3.3.x_to_3.3.1-Stable-Patch_Package.zip successfully downloaded.
+2014-06-13T02:11:20+00:00	INFO	update	Starting installation of new version.
+2014-06-13T02:11:24+00:00	INFO	update	Finalising installation.
+2014-06-13T02:11:24+00:00	INFO	update	Deleting removed files and folders.
+2014-06-13T02:11:25+00:00	INFO	update	Cleaning up after installation.
+2014-06-13T02:11:25+00:00	INFO	update	Update to version 3.3.1 is complete.
